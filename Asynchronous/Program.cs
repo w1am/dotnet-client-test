b8e6d9f553fcdc6e3442af6433dca46d37e1fec1
William Chong <william-chong@outlook.com>
2024-09-04 11:33:57 +0400

    test

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -22,12 +22,47 @@ namespace Asynchronous
                 Encoding.UTF8.GetBytes(JsonSerializer.Serialize(new { Message = "Hello, EventStore!" }))
             );
 
-            await client.AppendToStreamAsync(
+            var response = await client.AppendToStreamAsync(
                 streamName,
                 StreamState.Any,
                 [eventData]
             );
-            Console.WriteLine("Event appended successfully.");
+            Console.WriteLine($"Event appended successfully. {response.LogPosition}");
+
+            var subscription = await client.SubscribeToStreamAsync(
+                streamName,
+                FromStream.Start,
+                eventAppeared: async (subscription, evnt, cancellationToken) =>
+                {
+                    try
+                    {
+                        Console.WriteLine($"Received event: {evnt.OriginalEvent.EventType}");
+                        await HandleEvent(evnt);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error handling event: {ex.Message}");
+                    }
+                },
+                subscriptionDropped: SubscriptionDropped
+            );
+
+            Console.WriteLine("Subscription set up. Press any key to exit.");
+            Console.ReadKey();
+
+            subscription.Dispose();
+        }
+
+        private static void SubscriptionDropped(StreamSubscription subscription, SubscriptionDroppedReason reason,
+            Exception? arg3)
+        {
+            Console.WriteLine($"Subscription dropped: {reason}");
+        }
+
+        static Task HandleEvent(ResolvedEvent evnt)
+        {
+            Console.WriteLine($"Handling event: {evnt.OriginalEvent.EventType}");
+            return Task.CompletedTask;
         }
     }
 }
