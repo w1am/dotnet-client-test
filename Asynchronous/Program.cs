2ce7e588bf727cc0332b5dc6cc068fa2f82377b7
William Chong <william-chong@outlook.com>
2024-09-04 11:08:53 +0400

    Initialcommit

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -2,45 +2,32 @@
 using System.Text.Json;
 using EventStore.Client;
 
-namespace Testing;
-
-internal abstract class Program
+namespace Asynchronous
 {
-    private static async Task Main(string[] args)
+    internal abstract class Program
     {
-        // Connection settings
-        var settings = EventStoreClientSettings.Create("esdb://localhost:2113?tls=false");
-        var client = new EventStoreClient(settings);
-
-        var streamName = "example-stream";
-        var eventType = "sample-event";
-
-        // Create an event to append
-        var eventData = new EventData(
-            Uuid.NewUuid(),
-            eventType,
-            Encoding.UTF8.GetBytes(JsonSerializer.Serialize(new { Message = "Hello, EventStore!" }))
-        );
-
-        // Append the event to the stream
-        await client.AppendToStreamAsync(streamName, StreamState.Any, new[] { eventData });
-        Console.WriteLine("Event appended successfully.");
-
-
-        await using var subscription = client.SubscribeToStream(
-            streamName,
-            FromStream.Start);
-        await foreach (var message in subscription.Messages)
+        private static async Task Main(string[] args)
         {
-            switch (message)
-            {
-                case StreamMessage.Event(var evnt):
-                    Console.WriteLine($"Received event {evnt.OriginalEventNumber}@{evnt.OriginalStreamId}");
-                    break;
-                case StreamMessage.CaughtUp _:
-                    Console.WriteLine("Caught up to the stream.");
-                    break;
-            }
+            var settings =
+                EventStoreClientSettings.Create("esdb://admin:changeit@localhost:2113?tls=true&tlsVerifyCert=false");
+            settings.OperationOptions.ThrowOnAppendFailure = true;
+            var client = new EventStoreClient(settings);
+
+            var streamName = "example-stream";
+            var eventType = "sample-event";
+
+            var eventData = new EventData(
+                Uuid.NewUuid(),
+                eventType,
+                Encoding.UTF8.GetBytes(JsonSerializer.Serialize(new { Message = "Hello, EventStore!" }))
+            );
+
+            await client.AppendToStreamAsync(
+                streamName,
+                StreamState.Any,
+                [eventData]
+            );
+            Console.WriteLine("Event appended successfully.");
         }
     }
 }
